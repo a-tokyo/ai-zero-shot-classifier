8cbb05f1c13084260b1a74f1e3502af6623b999a
Tokyo <ahmed.tokyo1@gmail.com>
2025-05-30 13:15:19 +0100

    wip working

--- demo/next.config.ts
+++ demo/next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: 'export',
   distDir: 'dist',
+  experimental: {
+    externalDir: true,
+  },
   /* config options here */
 };
 
